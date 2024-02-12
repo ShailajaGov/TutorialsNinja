971bfe642c9cc09f409a81f5977248bc2c3664cd
ShailajaGov <159679031+ShailajaGov@users.noreply.github.com>
2024-02-11 20:50:04 -0800

    Update Utilities.java
    Updted implicit wait time and pageload wait time

--- src/main/java/com/tutorialsninja/utils/Utilities.java
+++ src/main/java/com/tutorialsninja/utils/Utilities.java
@@ -11,8 +11,8 @@ import org.openqa.selenium.io.FileHandler;
 
 public class Utilities {
 	
-	public static final int IMPLICIT_WAIT_TIME=10;
-	public static final int PAGELOAD_WAIT_TIME=15;
+	public static final int IMPLICIT_WAIT_TIME=15;
+	public static final int PAGELOAD_WAIT_TIME=20;
 	
 	public static String getTimeStamp()
 	{
